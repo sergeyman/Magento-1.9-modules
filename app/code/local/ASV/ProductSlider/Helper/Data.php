333b304adafc4205d007135868e4642ac4633dfc
sergey <sergeyman78@gmail.com>
2018-07-18 17:32:38 +0300

    clear master

---
+++ app/code/local/ASV/ProductSlider/Helper/Data.php
@@ -0,0 +1,8 @@
+<?php
+class Packt_Productslider_Helper_Data extends Mage_Core_Helper_Abstract {
+
+}
+
+/* We created a default
+helper, which we can call with the function Mage::helper('productslider').
+*/
