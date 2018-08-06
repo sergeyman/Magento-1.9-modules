b614b0745ba1e092181c8a2cf0f3c6cb9e9199bb
sergey <sergeyman78@gmail.com>
2018-08-06 21:11:07 +0300

    Packt_Productslider [Mg 1.8 Coockbook #10] init commit

---
+++ app/code/local/Packt/Productslider/Block/Catalog/Product/List.php
@@ -0,0 +1,23 @@
+<?php
+class Packt_Productslider_Block_Catalog_Product_List 
+extends Mage_Catalog_Block_Product_List 
+implements Mage_Widget_Block_Interface
+{
+    protected function _prepareLayout() 
+    {
+        $this->getLayout()->getBlock('head')->addCss('css/productslider.css');
+        
+        $this->getLayout()->getBlock('head')->addItem('skin_js', 'js/carouFredSel-6.2.1/jquery.carouFredSel-6.2.1-packed.js');
+        
+        parent::_prepareLayout();
+    }
+
+    public function getLoadedProductCollection()
+    {
+        return Mage::getModel('catalog/product')
+            ->getCollection()
+            ->addAttributeToSort('created_at', 'DESC')
+            ->addAttributeToSelect('*')
+            ->load();
+    }
+}
